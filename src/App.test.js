40994f8b762e991e32de2661c20db94f5a16e054
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2018-02-10 16:17:32 -0500

    Remove failing tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,8 +7,3 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-it('should fail twice', () => {
-  expect(true).toBe(false);
-  expect(false).toBe(true);
-});
